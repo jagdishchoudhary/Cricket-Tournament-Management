25a6972a79a4856aa9e667a7a44d457f27920fe1
jagdishchoudhary <54546696+jagdishchoudhary@users.noreply.github.com>
2021-08-19 20:05:01 +0530

    Update 123reset123.php

--- 123reset123.php
+++ 123reset123.php
@@ -1,8 +1,10 @@
 <?php 
-$dns="mysql:host=sql307.epizy.com;dbname=epiz_29301892_cricket";
-$username="epiz_29301892";
-$password="itoGnKNCI3w";
- $db= new PDO($dns, $username, $password);
+
+//connect your database
+
+
+//Coonection done
+
 //points table reset
 $query="DELETE FROM points_table WHERE 1;";
   $statement=$db->prepare($query);
